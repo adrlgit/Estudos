fa4dc02ff570f15f6d7e6aa9a2b558867b525eb3
André Luis <adrluis_mds@hotmail.com>
2017-09-20 09:41:24 -0300

    Adiciona ArrayList

---
+++ ArrayMultidimensional/src/com/andre/array/ArrayAprimoradoList.java
@@ -0,0 +1,15 @@
+package com.andre.array;
+
+import java.util.ArrayList;
+
+public class ArrayAprimoradoList {
+
+	public static void main(String[] args) {
+		ArrayList<String> nomes = new ArrayList<>();
+		
+		nomes.add("Andre");
+		
+		System.out.println(nomes.toString());
+	}
+
+}
